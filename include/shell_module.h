4579e2a123b30c464e8f45204a957f5f70e6f209
vlad_office <octal.s@gmail.com>
2013-12-02 04:10:49 +0200

    Updated the shell modules header

--- include/shell_module.h
+++ include/shell_module.h
@@ -13,4 +13,62 @@ typedef struct {
 
 } shell_callbacks_t;
 
+#ifndef HANDLE_USER_H
+
+/* Required functions */
+void	shell_init(shell_callbacks_t *cb);
+void	shell_read(void *data, unsigned int len);
+
+/* External functions */
+static	void		(*shell_write)(void *data, unsigned int len);
+static	int  		(*__shell_log)(int type, const char *module, const char *msg, ...);
+static	void		(*shell_exit)(void);
+
+
+/* Gloabal variables */
+const	static	char 	*ip_addr;
+const	static	char 	*username;
+static	unsigned int 	level;
+static	unsigned int	shell_printf_buf_len;
+
+
+/* Sets up all external and global objects according to data in cb  */
+#define SHELL_INIT_GLOBALS(cb) \
+	cb->shell_read = &shell_read; \
+	shell_write = cb->shell_write; \
+	__shell_log = cb->shell_log; \
+	shell_exit = cb->shell_exit; \
+	ip_addr = cb->ip_addr; \
+	username = cb->uname; \
+	level = cb->level; \
+	shell_printf_buf_len = 4096;
+
+
+enum LOG_TYPES {
+	LOG_MSG,
+	LOG_WARNING,
+	LOG_ERROR,
+	LOG_FATAL
+};
+
+#define		shell_log(module, msg...)		__shell_log(LOG_MSG, module, msg)
+#define		shell_log_warning(module, msg...)	__shell_log(LOG_WARNING, module, msg)
+#define		shell_log_error(module, msg...)		__shell_log(LOG_ERROR, module, msg)
+#define		shell_log_fatal(module, msg...)		__shell_log(LOG_FATAL, module, msg)
+
+
+static	void	shell_printf(const char *format, ...) {
+
+	va_list ap;
+	char *buf = alloca(shell_printf_buf_len);
+
+	va_start(ap, format);
+	vsnprintf(buf, shell_printf_buf_len, format, ap);
+	va_end(ap);
+
+	shell_write((void *)buf, strlen(buf));
+}
+
+#endif /* HANDLE_USER_H */
 #endif /* SHELL_MODULE_H */
+
--- src/shell_example.c
+++ src/shell_example.c
@@ -6,52 +6,24 @@
 
 #include "shell_module.h"
 
-
-static	void	(*shell_write)(void *data, unsigned int len);
-static	int  	(*shell_log)(int type, const char *module, const char *msg, ...);
-static	void	(*shell_exit)(void);
-static	void	shell_read(void *data, unsigned int len);
-
-const	static	char 	*ip_addr;
-const	static	char 	*username;
-static	unsigned int 	level;
-
-
 void	shell_init(shell_callbacks_t *cb) {
 
-	cb->shell_read = &shell_read;
-	shell_write = cb->shell_write;
-	shell_log = cb->shell_log;
-	shell_exit = cb->shell_exit;
-	ip_addr = cb->ip_addr;
-	username = cb->uname;
-	level = cb->level;
+	SHELL_INIT_GLOBALS(cb)
+	shell_log("Shell", "Example shell says hello!");
 }
 
-void	shell_printf(const char *format, ...) {
-
-	va_list ap;
-	char *buf = alloca(4096);
-
-	va_start(ap, format);
-	vsnprintf(buf, 4096, format, ap);
-	va_end(ap);
-
-	shell_write((void *)buf, strlen(buf));
-}
 
 void	shell_read(void *data, unsigned int len) {
 
 	char *buf = data;
 
-	shell_log(0, "Shell", "Hello!");
-
 	switch (buf[0]) {
 
 	  case 'q':
+		shell_log("Shell", "%s logout", username);
 		shell_exit();
 	  case 0xd:
-		shell_write("\r\n", 2);
+		shell_printf("\n\r");
 		break;
 	  default:
 		shell_printf("user %s : ip %s : level %u says: %.*s\r\n", username, ip_addr, level, len, data);
