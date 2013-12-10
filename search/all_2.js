d2e84c59851c4af6efe1c080dcaecb29b5d21380
vlad_home <octal.s@gmail.com>
2013-12-11 00:01:54 +0200

    Upadated documentation

--- search/all_2.js
+++ search/all_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['px',['px',['../structshell__info__struct.html#a6bbc6b7e52b17450c52d8e748d6abae1',1,'shell_info_struct']]]
+  ['px',['px',['../structshell__info__struct.html#a6bbc6b7e52b17450c52d8e748d6abae1',1,'shell_info_struct']]],
+  ['py',['py',['../structshell__info__struct.html#aa9511a47fd6271333948489a220e3029',1,'shell_info_struct']]]
 ];
--- search/related_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['shell_5fwrite',['shell_write',['../structshell__callbacks__t.html#aa52a2d3e4328cbbb82cb2bd3f4646551',1,'shell_callbacks_t']]]
-];
